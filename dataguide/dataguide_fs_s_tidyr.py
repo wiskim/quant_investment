0baf953a0d87623ecb4b7b283812d60962e7612e
wiskim <wiskimn@naver.com>
2021-05-02 19:24:56 +0900

    Pandas Series.str.replace의 regex를 명시적으로 True로 표시

--- dataguide/dataguide_fs_s_tidyr.py
+++ dataguide/dataguide_fs_s_tidyr.py
@@ -12,9 +12,9 @@ df.columns = ['stock_cd', 'stock_nm', 'closing_month', 'fiscal_year',
 df['con_div'] = 'Seperated'                                                     # 연결/별도 구분
 df['fiscal_quarter'] = ''                                                       # 분기 재무제표 구분
 df['unit'] = 1000                                                               # 재무제표 금액 단위
-df['item_nm'] = df.item_nm.str.replace('\(\*\)', '')                            # 계정과목명 정리
-df['item_nm'] = df.item_nm.str.replace('\*', '')                                # 계정과목명 정리
-df['item_nm'] = df.item_nm.str.replace('\(천원\)', '')                          # 계정과목명 정리
+df['item_nm'] = df.item_nm.str.replace('\(\*+\)', '', regex=True)               # 계정과목명 정리
+df['item_nm'] = df.item_nm.str.replace('\*', '', regex=True)                    # 계정과목명 정리
+df['item_nm'] = df.item_nm.str.replace('\(천원\)', '', regex=True)              # 계정과목명 정리
 df['item_cd'] = df.item_cd.str.slice(start=4)                                   # fs_item_info 테이블과 계정과목 코드 자릿수 일치
 df = df.loc[~df.item_nm.isin(['113903', '113904', '112503', '112504']), ]       # fs_item_info 테이블에 존재하지 않는 계정과목 삭제
 df = df[['con_div', 'stock_cd', 'stock_nm', 'closing_month', 'freq', 
